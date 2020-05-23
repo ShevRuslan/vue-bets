30267cf0aee266afbe3ee53395ce86abf497fb1f
ShevRuslan <sasa02sasa@mail.ru>
2020-05-23 11:56:04 +0600

    add base vue components

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-app-bar
+            app
+            color="indigo"
+            dark
+        >
+<!--            <v-app-bar-nav-icon @click.stop="close"></v-app-bar-nav-icon>-->
+            <v-toolbar-title>Ставки</v-toolbar-title>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            drawer: Boolean,
+        },
+        name: "Header",
+        methods: {
+            close: function() {
+                this.$emit('close');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
